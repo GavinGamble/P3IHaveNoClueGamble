9c60f3c8bc0fa47d30d5cfba99b520da6df50e44
GavinGamble <710145@student.dusd.net>
2023-05-18 12:13:05 -0700

    12.12

---
+++ P3FinalIHaveNoClueGamble/Assets/Scripts/RustyPlaneScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RustyPlaneScript : MonoBehaviour
+{
+
+    public float speed = 50f;
+    public float verticalInput;
+    public float rotationSpeed = 45.0f;
+    public float horizontalInput;
+    public float UpSpeed = 25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * verticalInput);
+            Debug.Log("Left Is Working");
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * verticalInput);
+            Debug.Log("Right Is Working");
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Debug.Log("Forward Is Working");
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Translate(Vector3.up * UpSpeed * Time.deltaTime);
+            Debug.Log("Up Is Working");
+
+            GetComponent<Rigidbody>().useGravity = false;
+
+        }
+    }
+}
