948d06aa100326ab18d3508926f36d7169fe7052
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2023-05-18 11:41:06 -0700

    Started Scripts On Planes

--- P3FinalIHaveNoClueGamble/Assets/Scripts/PlaneScript.cs
+++ P3FinalIHaveNoClueGamble/Assets/Scripts/PlaneScript.cs
@@ -9,6 +9,7 @@ public class PlaneScript : MonoBehaviour
     public float verticalInput;
     public float rotationSpeed = 45.0f;
     public float horizontalInput;
+    public float UpSpeed = 25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +24,29 @@ public class PlaneScript : MonoBehaviour
        if (Input.GetKey(KeyCode.A))
         {
              transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * verticalInput);
+            Debug.Log("Left Is Working");
         }
        
        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * verticalInput);
+            Debug.Log("Right Is Working"); 
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            
+            Debug.Log("Forward Is Working"); 
+        }
+       
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up* UpSpeed * Time.deltaTime);
+            Debug.Log("Up Is Working");
+
+            GetComponent<Rigidbody>().useGravity = false;
 
         }
-        
     }  
-}
+} 
