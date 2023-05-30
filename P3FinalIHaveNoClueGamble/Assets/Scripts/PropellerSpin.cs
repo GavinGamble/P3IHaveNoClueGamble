b26c9bbf5202ebc5e95ca3f222e182ac4a904df0
GavinGamble <710145@student.dusd.net>
2023-05-30 15:14:43 -0700

    5/30/2023 3:13Pm

--- P3FinalIHaveNoClueGamble/Assets/Scripts/PropellerSpin.cs
+++ P3FinalIHaveNoClueGamble/Assets/Scripts/PropellerSpin.cs
@@ -14,15 +14,17 @@ public class PropellerSpin : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-     
-        //transform.Rotate(Vector3.up * 40 * propeller );
-     if (Input.GetKey(KeyCode.Mouse0))
-     {
-             propeller = Input.GetAxis("Vertical");
-                transform.Rotate(Vector3.up * 40 * propeller);
-     }
-
+    {  
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+          propeller = Input.GetAxis("Vertical");
+          transform.Rotate(Vector3.up * 40 * propeller);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+           propeller = Input.GetAxis("Vertical");
+           transform.Rotate(Vector3.up * 40 * propeller);
+        }
     }
     
 
